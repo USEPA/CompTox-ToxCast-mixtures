88b8b784b943faefbbe6a6d2cc19d21182147aec
rabroughton <Broughton.Rachel@epa.gov>
2025-07-07 15:13:47 -0400

    updated mc5 loading organization and implemented weakly informative priors

--- scripts/curve_fitting_functions.R
+++ scripts/curve_fitting_functions.R
@@ -12,19 +12,14 @@ library(rjags)
 #' 
 #' @return dataframe of bootstraped resampled curve parameters for all parameters of the input curve shape type.
 #'  Returns NA if curve is inactive
-curve_bootstrap <- function(jj, curve.info, boot_num=1000, type="single",tc=FALSE){
+curve_bootstrap <- function(jj, curve.info, boot_num=1000, type="single"){
   if (curve.info$hitc[jj] >= 0.9){
     if (type=="single"){
       m4id <- curve.info$m4id[jj]
     } else if (type == "mix"){
       m4id <- curve.info$m4id_mix[jj]
     }
-    if (tc==FALSE){
-      comp_dat <- mc3 %>% filter(m3id %in% mc4_agg$m3id[which(mc4_agg$m4id==m4id)])
-    } else if (tc==TRUE){
-      # TC chemical library
-      comp_dat <- mc3_tcsubset %>% filter(m3id %in% mc4_agg_tcsubset$m3id[which(mc4_agg_tcsubset$m4id==m4id)])
-    }
+    comp_dat <- mc3 %>% filter(m3id %in% mc4_agg$m3id[which(mc4_agg$m4id==m4id)])
     bmad <- curve.info$bmad[jj]
     datchemval <- data.table(conc=comp_dat$conc, resp=comp_dat$resp, bmad=bmad)
     datchemstat = data.table(conc = sort(unique(datchemval$conc)))
@@ -53,6 +48,7 @@ curve_bootstrap <- function(jj, curve.info, boot_num=1000, type="single",tc=FALS
     datchemsample <- data.table(tempmat)
     # Fit each bootstrap sample using tcplFit
     fname <- paste0("fit", curve.info$modl[jj]) # requires each model function have name "fit____" where ____ is the model name
+    # use do.call to call fit function; cnst has different inputs than others.
     if(fname != "fitpoly2"){
       temp2 <- sapply(datchemsample,
                       match.fun(fname),
@@ -95,7 +91,7 @@ curve_bootstrap <- function(jj, curve.info, boot_num=1000, type="single",tc=FALS
 #'  
 #' @return dataframe of bayesian posterior parameter distribution samples for all parameters of the input curve shape type
 #'  Returns NA if input curve is inactive.
-bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single", poly2_correct=FALSE, tc=FALSE){
+bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=10000, type="single", poly2_correct=FALSE){
     if (curve.info$hitc[ii]<0.9){
       bayesparams <- NA
     } else {
@@ -107,27 +103,17 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
       # # define the data
       if (poly2_correct){
         single_dat <- curve.info
-        if (tc){
-          comp_dat <- mc3_tcsubset %>% filter(m3id %in% mc4_agg_tcsubset$m3id[which(mc4_agg_tcsubset$m4id==m4id_curve)])
-        } else if (!tc){
-          comp_dat <- mc3 %>% filter(m3id %in% mc4_agg$m3id[which(mc4_agg$m4id==m4id_curve)])
-        }
+        comp_dat <- mc3 %>% filter(m3id %in% mc4_agg$m3id[which(mc4_agg$m4id==m4id_curve)])
       } else{
-        if (tc){
-          single_dat <- mc5_attg_endpts[m4id==m4id_curve,]
-          comp_dat <- mc3_tcsubset %>% filter(m3id %in% mc4_agg_tcsubset$m3id[which(mc4_agg_tcsubset$m4id==m4id_curve)])
-        } else if (!tc){
-          single_dat <- mc5[m4id==m4id_curve,]
-          comp_dat <- mc3 %>% filter(m3id %in% mc4_agg$m3id[which(mc4_agg$m4id==m4id_curve)])
-        }
+        single_dat <- mc5[m4id==m4id_curve,]
+        comp_dat <- mc3 %>% filter(m3id %in% mc4_agg$m3id[which(mc4_agg$m4id==m4id_curve)])
       }
       # medians
       rmds <- tapply(comp_dat$resp, comp_dat$conc, median)
-      # For each curve type: (1) define bayesian likelihood model and (2) define prior as uniform with same bounds as tcplfit2
+      # For each curve type: (1) define bayesian likelihood model and (2) define prior as uniform with same bounds as ToxCast
       if (single_dat$modl=="poly1"){
         # define the model
-        a0 <- max(abs(rmds))/max(comp_dat$conc)
-        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat), a_low=-1e8*a0, a_upp=1e8*a0)
+        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat))
         model_string <- textConnection("model{
                                        # Likelihood
                                        for(i in 1:n){
@@ -138,16 +124,30 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
                                        k <- 4
                                        tau ~ dgamma(0.1,0.1)
                                        sigma <- 1/sqrt(tau)
-                                       a ~ dunif(a_low, a_upp)
+                                       a ~ dnorm(0,1)
                                        }")
         # define the initial values
         inits <- list(a=single_dat$a, tau=(1/exp(single_dat$er))^2)
         params <- c("a", "sigma")
       } else if (single_dat$modl=="poly2"){
-        a0 <- max(abs(rmds))
-        b0 <- max(comp_dat$conc)
-        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat), b1_low=-1e8*a0,
-                     b1_upp=1e8*a0, b2_low=-1e8*b0, b2_upp=1e8*b0)
+        # data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat))
+        # model_string <- textConnection("model{
+        #                                # Likelihood
+        #                                for(i in 1:n){
+        #                                 resp[i] ~ dt(mu[i],(1/(sigma^2)),k)
+        #                                 mu[i] <- b1*conc[i] + b2*(conc[i]*conc[i])
+        #                                }
+        #                                # Priors
+        #                                k <- 4
+        #                                sigma ~ dt(0,(2.5^(-2)),1)
+        #                                b1 ~ dnorm(0,1)
+        #                                b2 ~ dnorm(0,1)
+        #                                }")
+        # # define the initial values
+        # inits <- list(b1=single_dat$a/single_dat$b, b2=single_dat$a/(single_dat$b^2), sigma=exp(single_dat$er))
+        # params <- c("b1", "b2", "sigma")
+        
+        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat))
         model_string <- textConnection("model{
                                        # Likelihood
                                        for(i in 1:n){
@@ -158,18 +158,14 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
                                        k <- 4
                                        tau ~ dgamma(0.1,0.1)
                                        sigma <- 1/sqrt(tau)
-                                       b1 ~ dunif(b1_low, b1_upp)
-                                       b2 ~ dunif(b2_low, b2_upp)
+                                       b1 ~ dnorm(0,1)
+                                       b2 ~ dnorm(0,1)
                                        }")
         # define the initial values
         inits <- list(b1=single_dat$a/single_dat$b, b2=single_dat$a/(single_dat$b^2), tau=(1/exp(single_dat$er))^2)
         params <- c("b1", "b2", "sigma")
       } else if (single_dat$modl=="pow"){
-        a0 <- max(abs(rmds))
-        b0 <- max(comp_dat$conc)
-        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat),
-                     a_low=-1e8*a0, a_upp=1e8*a0)
-        # p bound set 0.3-20 in ToxCast
+        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat))
         model_string <- textConnection("model{
                                        # Likelihood
                                        for(i in 1:n){
@@ -180,21 +176,16 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
                                        k <- 4
                                        tau ~ dgamma(0.1,0.1)
                                        sigma <- 1/sqrt(tau)
-                                       a ~ dunif(a_low, a_upp)
-                                       p ~ dunif(0.3, 20)
+                                       a ~ dlnorm(0,1)
+                                       p ~ dlnorm(0,1)
                                        }")
         # define the initial values
         inits <- list(a=single_dat$a, p=single_dat$p, tau=(1/exp(single_dat$er))^2)
         params <- c("a", "p", "sigma")
       } else if (single_dat$modl=="hill"){
-        tp_val <- 1.2*max(abs(min(comp_dat$resp)),abs(max(comp_dat$resp)))
-        ga_low <- min(comp_dat$conc)/10
-        ga_upp <- max(comp_dat$conc)*(10^0.5)
-        # p bounds set in TOxCast of 0.3 - 8
         # for hill, concentrations are internally converted to log in ToxCast
-        # here, they are kept as is
-        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat),
-                     tp_val=tp_val,ga_low=ga_low, ga_upp=ga_upp)
+        # here, keep it not in log scale
+        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat))
         model_string <- textConnection("model{
                                        # Likelihood
                                        for(i in 1:n){
@@ -205,27 +196,17 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
                                        k <- 4
                                        tau ~ dgamma(0.1,0.1)
                                        sigma <- 1/sqrt(tau)
-                                       ga ~ dunif(ga_low, ga_upp)
-                                       tp ~ dunif(-tp_val, tp_val)
-                                       p ~ dunif(0.3, 8)
+                                       ga ~ dlnorm(1,1)
+                                       tp ~ dlnorm(0,1)
+                                       p ~ dlnorm(0,1)
                                        }")
         # define the initial values
         inits <- list(ga=single_dat$ga, tp=single_dat$tp, p=single_dat$p, tau=(1/exp(single_dat$er))^2)
         params <- c("ga", "tp", "p", "sigma")
       } else if (single_dat$modl=="gnls"){
-        tp_val <- 1.2*max(abs(min(comp_dat$resp)),abs(max(comp_dat$resp)))
-        ga_low <- min(comp_dat$conc)/10
-        #ga_upp <- max(comp_dat$conc)*(10^0.5)
-        ga_upp <- max(comp_dat$conc) # change from ToxCast bounds
-        #la_low <- min(comp_dat$conc)/10
-        la_low <- min(comp_dat$conc) # change from ToxCast bounds
-        #la_upp <- max(comp_dat$conc)*(10^2)
-        la_upp <- max(comp_dat$conc)*(50) # change from ToxCast bounds
-        # p and q bounds set in TOxCast of 0.3 - 8
-        # for gnls, concentrations are internally converted to log in ToxCast
-        # here, keep concentrations as is
-        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat),
-                     tp_val=tp_val,ga_low=ga_low, ga_upp=ga_upp, la_low=la_low, la_upp=la_upp)
+        # # for gnls, concentrations are internally converted to log in ToxCast
+        # # here, we just keep it in non-log space for simplicity
+        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat))
         model_string <- textConnection("model{
                                        # Likelihood
                                        for(i in 1:n){
@@ -236,20 +217,17 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
                                        k <- 4
                                        tau ~ dgamma(0.1,0.1)
                                        sigma <- 1/sqrt(tau)
-                                       la ~ dunif(la_low, la_upp)
-                                       tp ~ dunif(-tp_val, tp_val)
-                                       p ~ dunif(0.3, 8)
-                                       q ~ dunif(0.3, 8)
-                                       ga ~ dunif(ga_low, ga_upp)
+                                       ga ~ dlnorm(1,1)
+                                       la ~ dlnorm(3,1)
+                                       tp ~ dlnorm(0,1)
+                                       p ~ dlnorm(0,1)
+                                       q ~ dlnorm(0,1)
                                        }")
         # define the initial values
         inits <- list(ga=single_dat$ga, tp=single_dat$tp, p=single_dat$p, q=single_dat$q, la=single_dat$la, tau=(1/exp(single_dat$er))^2)
         params <- c("ga", "tp", "p", "q", "la", "sigma")
       } else if (single_dat$modl=="exp2"){
-        a0 <- max(abs(rmds))
-        b0 <- max(comp_dat$conc)
-        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat), a_low=-1e8*a0,
-                     a_upp=1e8*a0, b_low=1e-2*b0, b_upp=1e8*b0)
+        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat))
         model_string <- textConnection("model{
                                        # Likelihood
                                        for(i in 1:n){
@@ -260,18 +238,14 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
                                        k <- 4
                                        tau ~ dgamma(0.1,0.1)
                                        sigma <- 1/sqrt(tau)
-                                       a ~ dunif(a_low, a_upp)
-                                       b ~ dunif(b_low, b_upp)
+                                       a ~ dlnorm(0,1)
+                                       b ~ dlnorm(0,1)
                                        }")
         # define the initial values
         inits <- list(a=single_dat$a, b=single_dat$b, tau=(1/exp(single_dat$er))^2)
         params <- c("a", "b", "sigma")
       } else if (single_dat$modl=="exp3"){
-        a0 <- max(abs(rmds))
-        b0 <- max(comp_dat$conc)
-        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat), a_low=-1e8*a0,
-                     a_upp=1e8*a0, b_low=1e-2*b0, b_upp=1e8*b0)
-        # p bounds are set to 0.3-8 in toxcast
+        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat))
         model_string <- textConnection("model{
                                        # Likelihood
                                        for(i in 1:n){
@@ -282,20 +256,15 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
                                        k <- 4
                                        tau ~ dgamma(0.1,0.1)
                                        sigma <- 1/sqrt(tau)
-                                       a ~ dunif(a_low,a_upp)
-                                       b ~ dunif(b_low,b_upp)
-                                       p ~ dunif(0.3, 8)
+                                       a ~ dlnorm(0,1)
+                                       b ~ dlnorm(0,1)
+                                       p ~ dlnorm(0,1)
                                        }")
         # define the initial values
         inits <- list(a=single_dat$a, b=single_dat$b, p=single_dat$p, tau=(1/exp(single_dat$er))^2)
         params <- c("a", "b", "p", "sigma")
       } else if (single_dat$modl=="exp4"){
-        a0 <- max(abs(rmds))
-        ga_low <- min(comp_dat$conc)/10
-        ga_upp <- max(comp_dat$conc)*(10^0.5)
-        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat), tp_low= -1.2*a0,
-                     tp_upp=1.2*a0, ga_low=ga_low, ga_upp=ga_upp)
-        # p bounds are set to 0.3-8 in toxcast
+        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat))
         model_string <- textConnection("model{
                                        # Likelihood
                                        for(i in 1:n){
@@ -306,18 +275,14 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
                                        k <- 4
                                        tau ~ dgamma(0.1,0.1)
                                        sigma <- 1/sqrt(tau)
-                                       ga ~ dunif(ga_low, ga_upp)
-                                       tp ~  dunif(tp_low, tp_upp)
+                                       ga ~ dlnorm(1,1)
+                                       tp ~ dlnorm(0,1)
                                        }")
         # define the initial values
         inits <- list(ga=single_dat$ga, tp=single_dat$tp, tau=(1/exp(single_dat$er))^2)
         params <- c("ga", "tp", "sigma")
       } else if (single_dat$modl=="exp5"){
-        a0 <- max(abs(rmds))
-        ga_low <- min(comp_dat$conc)/10
-        ga_upp <- max(comp_dat$conc)*(10^0.5)
-        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat), tp_low= -1.2*a0,
-                     tp_upp=1.2*a0, ga_low=ga_low, ga_upp=ga_upp)
+        data <- list(conc=comp_dat$conc, resp=comp_dat$resp, n=nrow(comp_dat))        
         model_string <- textConnection("model{
                                        # Likelihood
                                        for(i in 1:n){
@@ -328,9 +293,9 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
                                        k <- 4
                                        tau ~ dgamma(0.1,0.1)
                                        sigma <- 1/sqrt(tau)
-                                       ga ~ dunif(ga_low, ga_upp)
-                                       tp ~  dunif(tp_low, tp_upp)
-                                       p ~ dunif(0.3, 8)
+                                       ga ~ dlnorm(1,1)
+                                       tp ~ dlnorm(0,1)
+                                       p ~ dlnorm(0,1)
                                        }")
         # define the initial values
         inits <- list(ga=single_dat$ga, tp=single_dat$tp, p=single_dat$p, tau=(1/exp(single_dat$er))^2)
@@ -339,6 +304,7 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
       
       # define model
       model <- jags.model(model_string, data=data, inits=inits, n.chains=1, quiet=TRUE)
+      # model <- jags.model(model_string, data=data, inits=inits, n.chains=3, quiet=TRUE)
       # using one chain, but can use more to evaluate convergence
       
       # run burn-in
@@ -347,9 +313,10 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
       # run samples
       samples <- coda.samples(model, variable.names=params, n.iter=n_iter, progress.bar="none")
       
-      #summary(samples)
+      # summary(samples)
+      # plot(samples, trace=TRUE,density=FALSE)
+      # gelman.diag(samples)
       
-      #plot(samples)
       # convert poly2 back to a and b parameters
       if (single_dat$modl=="poly2"){
         poly2_temp <- data.frame(samples[[1]])
@@ -361,6 +328,6 @@ bayes_curve <- function(ii, curve.info, n_iter=10000, n_burn=2000, type="single"
         bayesparams <- data.frame(samples[[1]])     
       }
     }
-    
+  
   return(bayesparams)
 }
